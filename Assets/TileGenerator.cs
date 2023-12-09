a384457c5d1a17815c247bea4383664110b57d35
Sarvottam <sarvottamswaroop100@gmail.com>
2023-12-09 13:08:05 +0530

    Added collectibles
    - Added a star collectible
    - Added sound to it
    - It just gets destroyed on trigger right now doesn't do anything

--- Assets/TileGenerator.cs
+++ Assets/TileGenerator.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 public class TileGenerator : MonoBehaviour
 {
     public GameObject tilePrefab; // Reference to the tile prefab
     public int numRows = 10; // Number of rows
     public int numColumns = 10; // Number of columns
     public float distanceBetweenTiles = 10f; // Distance between tiles
+    
+    public GameObject starPrefab; // Reference to the star prefab
+    //Offset for position above tile
+    private float offset ;
 
     void Start()
     {
+        offset = starPrefab.transform.localScale.y / 2;
         GenerateTiles();
     }
 
@@ -20,6 +25,11 @@ public class TileGenerator : MonoBehaviour
             {
                 Vector3 tilePosition = new Vector3(col * distanceBetweenTiles, 0f, row * distanceBetweenTiles);
                 Instantiate(tilePrefab, tilePosition, Quaternion.identity, transform);
+                //5% probability of instantiating a star
+                if (Random.Range(0f, 1f) < 0.05f)
+                {
+                    Instantiate(starPrefab, tilePosition + Vector3.up*offset, Quaternion.identity, transform);
+                }
             }
         }
     }
